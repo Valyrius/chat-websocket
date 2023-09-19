1020e187c681ee3fc1fae861e27f716c17ed3e9d
Valyrius <51477242+Valyrius@users.noreply.github.com>
2023-09-19 03:59:49 +0300

    Init Push

---
+++ go.mod
@@ -0,0 +1,7 @@
+module chat-websocket
+
+go 1.21.1
+
+require (
+	github.com/gorilla/websocket v1.5.0
+)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+    "log"
+    "time"
+    "html"
+    "net/http"
+
+    "github.com/gorilla/websocket"
+)
+
+var (
+    upgrader = websocket.Upgrader{
+        CheckOrigin: func(r *http.Request) bool {
+            return true
+        },
+    }
+
+    connected = make(map[*websocket.Conn]bool)
+)
+
+type Message struct {
+    Username      string `json:"username"`
+    UsernameColor string `json:"username_color"`
+    Message       string `json:"message"`
+    Timestamp     int64  `json:"timestamp"`
+}
+
+
+func main() {
+    log.Println("starting server on :8765")
+    http.HandleFunc("/", handleConnection)
+    err := http.ListenAndServe(":8765", nil)
+    if err != nil {
+        log.Fatal("Error starting WebSocket server: ", err)
+    }
+}
+
+
+func sendMessage(socket *websocket.Conn, message Message) {
+    err := socket.WriteJSON(message)
+    if err != nil {
+        socket.Close()
+        delete(connected, socket)
+        log.Println(err)
+    }
+}
+
+
+func broadcastMessage(message Message) {
+    for client := range connected {
+        go sendMessage(client, message)
+    }
+}
+
+
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+    defer conn.Close()
+
+    connected[conn] = true
+
+    for {
+        message := Message{}
+        err := conn.ReadJSON(&message)
+        if err != nil {
+            delete(connected, conn)
+            return
+        }
+
+        message.Message = html.EscapeString(message.Message)
+        message.Timestamp = time.Now().Unix()
+
+        broadcastMessage(message)
+    }
+}
+
